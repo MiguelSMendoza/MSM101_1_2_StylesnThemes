4d86dc8918b7d13dbb46918b0ecf927c02e9ab3d
Miguel S. Mendoza <misanmen@gmail.com>
2012-09-24 19:00:23 +0200

    First

---
+++ src/es/netrunners/stylesnthemes/StylesnThemesActivity.java
@@ -0,0 +1,13 @@
+package es.netrunners.stylesnthemes;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StylesnThemesActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
